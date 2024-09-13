b3470e0f4440571f958f88a3bc7b57063450a6a3
hungms <140624966+hungms@users.noreply.github.com>
2024-09-14 02:43:19 +0800

    update readme

--- requirement.R
+++ requirement.R
@@ -1,28 +1,3 @@
-# Installation
-
-
-## Install CRAN packages
-options(timeout = 500)
-cran <- c(
-    "ggplot2", "dplyr", "ggrepel", "patchwork", "cowplot", "scales", 
-    "grid", "gridExtra", "viridis", "wesanderson", "RColorBrewer", "qs", 
-    "readxl", "writexl", "rstatix", "ggpubr", "Seurat", "harmony")
-install.packages(cran)
-
-## Install Bioconductor packages
-bioc <- c("biomaRt", "BiocParallel", "UCell", "celda", "scDblFinder", "ComplexHeatmap")
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install(bioc)
-
-## Install Github packages
-git <- c("chris-mcginnis-ucsf/DoubletFinder", "mojaveazure/seurat-disk")
-devtools::install_github(git)
-
-## Optional Modules
-### Under Development (as of 31 Aug 2024)
-
-
 ## Boot
 # roxygen2::roxygenise()
 # devtools::document()
